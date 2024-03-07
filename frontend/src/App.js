5b7fe50b533d1dbcc183e79cc319f218a9bbc6a4
AyoubHVA <ayoub.ez-zaouia@hva.nl>
2024-03-07 01:36:40 +0100

    frontend refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
 import ClientSelection from './components/ClientSelection';
 import ProspectList from './components/ProspectList';
 import ProspectLandingPage from './components/ProspectLandingPage';
 
 function App() {
+  const [selectedClientId, setSelectedClientId] = useState('');
+  const [selectedProspectId, setSelectedProspectId] = useState('');
+
+  const handleClientSelect = (clientId) => {
+    setSelectedClientId(clientId);
+    // Reset selected prospect when changing clients
+    setSelectedProspectId('');
+  };
+
+  const handleProspectSelect = (prospectId) => {
+    setSelectedProspectId(prospectId);
+  };
+
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" exact>
-            <ClientSelection />
-          </Route>
-          <Route path="/prospects/:clientId" exact render={({ match }) => (
-            <ProspectList clientId={match.params.clientId} />
-          )} />
-          <Route path="/prospect/:prospectId" exact render={({ match }) => (
-            <ProspectLandingPage prospectId={match.params.prospectId} />
-          )} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Select a Client</h1>
+      <ClientSelection onClientSelect={handleClientSelect} />
+
+      {selectedClientId && (
+        <ProspectList clientId={selectedClientId} onProspectSelect={handleProspectSelect} />
+      )}
+
+      {selectedProspectId && (
+        <ProspectLandingPage prospectId={selectedProspectId} />
+      )}
+    </div>
   );
 }
 
